3d3aea1e57b82880e224704723fc443af89c382f
Sam Marcellus <sam.marcellus@gmail.com>
2019-03-04 18:12:38 -0800

    Update widget.js

--- widget.js
+++ widget.js
@@ -2,7 +2,7 @@ function init() {
   var container = document.getElementById("SiteNav");
   var content = document.createElement("li");
   content.style.color = "red";
-  content.innerHTML = "Hello";
+  content.innerHTML = "SpendGo Rewards <span class='rewardsValue'></span>";
   container.appendChild(content);
 }
 
